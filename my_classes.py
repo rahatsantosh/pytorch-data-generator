da46fa981c425a08eb8176b9613ec8fb3f14b7b1
Rahat Santosh <29304369+rahatsantosh@users.noreply.github.com>
2020-04-16 15:37:05 +0530

    Include flow_from_directory

--- my_classes.py
+++ my_classes.py
@@ -1,4 +1,7 @@
 import torch
+import os
+from PIL import Image
+import numpy as np
 from torch.utils import data
 
 class Dataset(data.Dataset):
@@ -22,3 +25,32 @@ class Dataset(data.Dataset):
        y = self.labels[ID]
 
        return X, y
+
+
+class Dataset2(data.Dataset):
+   def __init__(self, root):
+       self.path_list = []
+       self.label_list = []
+       count = 0
+       dir_list = [x[1] for x in os.walk(root)][0]
+       labels = np.arange(len(dir_list))
+       label_dict = {dir_list[i]:labels[i] for i in range(len(dir_list))}
+       for a in dir_list:
+           d = os.path.join(root,a)
+           for path in os.listdir(d):
+                if os.path.isfile(os.path.join(d, path)):
+                    count += 1
+                    self.path_list.append(os.path.join(d, path))
+                    self.label_list.append(label_dict[a])
+       self.length = count
+
+   def __len__(self):
+       return self.length
+
+   def __getitem__(self, index, transform=None):
+       img = Image.open(self.path_list[index])
+       if transform:
+          x = transform(img)
+       y = self.label_list[index]
+
+       return x, y
